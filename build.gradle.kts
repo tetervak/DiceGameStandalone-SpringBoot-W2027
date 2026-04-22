bdef97140f91efd6c547b538a383ee7b8c5e78d3
Alex Tetervak <tetervak@sheridancollege.ca>
2026-04-22 16:39:03 -0400

    removed mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,26 +24,18 @@ repositories {
     mavenCentral()
 }
 
-// Add mockito-core to the mockitoAgent configuration
-val mockitoAgent by configurations.creating {
-    isTransitive = false
-}
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    // Add the mockito-core to the mockitoAgent configuration
-    mockitoAgent("org.mockito:mockito-core")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-    // Add the javaagent JVM argument
-    jvmArgs("-javaagent:${mockitoAgent.asPath}")
 }
